58c54e993c9c5635a01021ae1bf402e9f8deaf25
Elena <eispravnikova@gpm-rtv.ru>
2020-10-26 03:49:46 +0300

    активация видноплеера при наведение

---
+++ src/components/video/video.jsx
@@ -0,0 +1,42 @@
+import React, {PureComponent, createRef} from "react";
+import PropTypes from "prop-types";
+
+class Video extends PureComponent {
+  constructor(props) {
+    super(props);
+    this._videoRef = createRef();
+  }
+
+  // обновляем состоние тега видео
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+
+    if (this.props.isActive) {
+      video.play();
+    } else {
+      video.src = this.props.videoSrc;
+      video.currentTime = 0;
+    }
+  }
+
+  render() {
+    const {posterSrc, videoSrc} = this.props;
+    return (
+      <video
+        className="player__video"
+        muted
+        poster={posterSrc}
+        src={videoSrc}
+        ref={this._videoRef}
+      />
+    );
+  }
+}
+
+Video.propTypes = {
+  posterSrc: PropTypes.string.isRequired,
+  videoSrc: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired
+};
+
+export default Video;
